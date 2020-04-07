4a94ff0ce93bd13f2608767637291ccec81ac1f5
mech0k <trooperv99@gmail.com>
2020-04-07 19:43:43 +0300

    improvements

--- LeetCode/group_anagrams.cpp
+++ LeetCode/group_anagrams.cpp
@@ -2,49 +2,18 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-string order(string str){
-    sort(str.begin(), str.end()); 
-    return str;
-}
-
 vector<vector<string>> groupAnagrams(vector<string>& strs){
-    unordered_map<string, vector<string>> words;
-    for(size_t i = 0; i < strs.size(); i++){
-        string temp = order(strs[i]);
-        auto g = words.find(temp);
-        if(g != words.end()){
-            words.at(temp).push_back(strs[i]);
-        }else{
-            pair<string, vector<string>> t;
-            t.first = temp;
-            vector<string> te;
-            te.push_back(strs[i]);
-            t.second = te;
-            words.insert(t);
-        }
-
-    }
-
     vector<vector<string>> ans;
-
-    for(auto it = words.begin(); it != words.end(); ++it){
-        vector<string> temp_v;
-        for(auto &i: it->second){
-            temp_v.push_back(i);
-        }
-        ans.push_back(temp_v);
+    unordered_map<string, vector<string>> m;
+    for(string &str : strs){
+        string temp = str;
+        sort(temp.begin(), temp.end());
+        m[temp].push_back(str);
     }
-
+    for(auto i: m){
+        ans.push_back(i.second);
+    }
     return ans;
-    //cout << endl;
-    //for(auto it = words.begin(); it != words.end(); ++it){
-    //    cout << " " << it->first << ": ";
-    //    for(auto &i: it->second){
-    //        cout << i << " ";
-    //    }
-    //    cout << endl;
-    //}
 }
 
 int main(){
