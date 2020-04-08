ded84ca0bd9baa6982bbd1aa3dd4b7a1ed5be344
mech0k <trooperv99@gmail.com>
2020-04-08 13:14:01 +0300

    middle node

---
+++ LeetCode/middle_node.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {};
+};
+
+
+ListNode* middleNode(ListNode* head){
+    ListNode* ans = head;
+    int count = 0;
+    while(head->next != NULL){
+        head = head->next;
+        count++;
+    }
+    count++;
+    for(int i = 0; i < count / 2; i++){
+        ans = ans->next;
+    }
+    return ans;
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
